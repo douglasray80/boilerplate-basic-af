a74d1256c093936dd2e71efe39d72cb11db958d5
Douglas Ray <dpr980@gmail.com>
2019-03-27 13:27:33 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static('dist'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+  console.log("--------------------------");
+});
+
